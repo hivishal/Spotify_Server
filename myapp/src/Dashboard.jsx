a1c113095e6f985657c2aedc915696faa1dcefc6
vishal <vishalgautam0312@gmail.com>
2024-11-06 22:05:20 +0530

    serious shit

--- myapp/src/Dashboard.jsx
+++ myapp/src/Dashboard.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 
 export default function Dashboard(){
-    return(
-        <>
-        <h1>
-            hello wrold!
-        </h1>
-        </>
-    )
+    const q = new URLSearchParams(window.location.search).get('token');
+    const token = decodeURIComponent(q);
+    const a = JSON.parse(token);
+    const [player, setPlayer] = useState(undefined);
+    
+useEffect(() => {
+
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+    
+
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+
+        const player = new window.Spotify.Player({
+            name: 'Web Playback SDK',
+            getOAuthToken: cb => { cb(a.access_token); },
+            volume: 0.5
+        });
+        setPlayer(player);
+
+
+        player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+        });
+
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+
+
+        player.connect();
+
+    };
+}, [a.access_token]);
+
+    return (
+        <div>
+            {a.access_token}
+        </div>
+    );
 }
