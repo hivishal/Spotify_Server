8a2f871824de27b56dfdc7b7f8ed8ceb581a55a1
vishal <vishalgautam0312@gmail.com>
2024-11-06 22:05:59 +0530

    server's cooking

--- Server/index.js
+++ Server/index.js
@@ -1,24 +1,53 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import Post from './Module.js'
+import axios from 'axios';
 const port = 3001 ;
 const app = express();
+const client_id = process.env.CLIENT_ID
+const client_secret = process.env.CLIENT_SECRET
 
-var token = {} ;
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`) ;
-});
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/login',async(req,res)=>{
     try{
-       const code = req.query.code ;
-        token = await Post(code) ;
-        res.redirect('http://localhost:3000/Dashboard');[]
-    }catch{
+        const code = req.query.code ;
+        const obj = await Post(code) ;
+        obj.code = code ;
+        const token = encodeURIComponent(JSON.stringify(obj));
+        res.redirect(`http://localhost:3000/Dashboard?token=${token}`);
+    }catch(error){
         res.send("Internal Server Error from post!");
+        console.log(error);
     }
-    
 });
 
-app.get('/Token',(req,res)=>{
-    res.send(token);
-})
+
+app.post('/refresh',async(req,res)=>{
+    const { refresh_token } = req.body;  
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'refresh_token');
+    params.append('refresh_token', refresh_token);
+
+    try {
+        const refresh = await axios.post("https://accounts.spotify.com/api/token", params.toString(), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+            }
+        });
+        console.log(refresh);
+        res.json(refresh.data);}
+        
+        catch(error){
+            console.error(error);
+        }
+});
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`) ;
+});
