f93f3ea29cd771bfcfdc6dd32310d37a4e622a08
vishal <vishalgautam0312@gmail.com>
2024-11-20 18:24:50 +0530

    successfuly transferred the playback to spotify

--- myapp/src/Dashboard.jsx
+++ myapp/src/Dashboard.jsx
@@ -6,7 +6,8 @@ export default function Dashboard(){
     const token = decodeURIComponent(q);
     const a = JSON.parse(token);
     const [player, setPlayer] = useState(undefined);
-    const [isready,setready] = useState(false);
+    const [device , setdevice] = useState(undefined);
+   
 
     useEffect(() => {
         const script = document.createElement("script");
@@ -27,6 +28,7 @@ export default function Dashboard(){
     
             player.addListener('ready', ({ device_id }) => {
                 console.log('Ready with Device ID', device_id);
+                setdevice(device_id);
             });
     
             player.addListener('not_ready', ({ device_id }) => {
@@ -35,13 +37,41 @@ export default function Dashboard(){
     
     
            await player.connect();
-           setready(true);
 
         
         };
     }, [a.access_token]);
     
 //since the isready flag is set to true keep the if else statement ready to render the music player
+
+        const transferPlayback = async (accessToken, device) => {
+            const url = 'https://api.spotify.com/v1/me/player';
+            const body = {
+                device_ids: [device],
+                play: true,
+            };
+        
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            });
+        
+            if (response.ok) {
+                console.log('Playback transferred successfully');
+            } else {
+                const error = await response.json();
+                console.error('Error transferring playback:', error);
+            }
+        };
+
+if(player){
+    transferPlayback(a.access_token,device);
+}
+    
     return (
         <div className='child'>
             <h1 style={{ color: '#1ed760' }}>gello world</h1>
